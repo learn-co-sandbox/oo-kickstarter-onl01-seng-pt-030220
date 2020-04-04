aec675fec208c7800ea500fb7a424623d2f31ef1
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 23:26:13 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,23 +12,3 @@ class Backer
   end
 end
 
-# class Backer 
-  
-#   attr_reader :name
-  
-  
-  
-#   def intialized(backed_projects)
-#     @name = name 
-#     # @project = project
-#     @backed_projects = []
-#   end 
-  
-#   def add_backer(name)
-#     # b1 = backer.new(" ")
-#     @backed_projects << self
-#   end 
-# end 
-
-# backer = Backer.new("Avi")
-# backer.name 
