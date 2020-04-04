c897eca14699c1d241bcea3b23e428b231794da7
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 23:24:13 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,17 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+end
+
 # class Project
 #   attr_reader :name, :title
 #   @backers = []
